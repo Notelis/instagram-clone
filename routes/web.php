d046409d0e0939946d9a92ad234cae5e129651b3
Notelis <rafeltheng@gmail.com>
2025-06-02 11:02:09 +0700

    Added Photo upload
    Access with /upload and /feed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PhotoController;
+use App\Models\Photo;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/upload', function () {
+    return view('upload');
+});
+
+Route::post('/photos', [PhotoController::class, 'store'])->name('photos.store');
+
+Route::get('/feed', function () {
+    $photos = Photo::latest()->get(); // newest first
+    return view('photos.feed', compact('photos'));
+})->name('photos.feed');
+Route::get('/photos/{photo}', function (Photo $photo) {
+    return view('photos.feed', compact('photo'));
+})->name('photos.feed');

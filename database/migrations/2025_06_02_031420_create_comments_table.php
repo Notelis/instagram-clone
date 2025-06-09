ab1f1f9b094360817a285dac56166a635591d2fd
WinnyJazz <jewinn2244@gmail.com>
2025-06-09 16:39:46 +0700

    menambahkan bagian schema comments

--- database/migrations/2025_06_02_031420_create_comments_table.php
+++ database/migrations/2025_06_02_031420_create_comments_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
-            $table->foreignId('photo_id')->constrained()->onDelete('cascade'); 
-            $table->text('comment_text'); 
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('photo_id')->constrained()->onDelete('cascade'); // asumsi post = photo
+            $table->text('comment_text');
             $table->timestamps();
         });
     }

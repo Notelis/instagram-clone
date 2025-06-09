7fc5f2d538dc7471b9a36bed0f61518f5b811cbd
WinnyJazz <jewinn2244@gmail.com>
2025-06-09 16:38:39 +0700

    menambahkan isi Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,23 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    use HasFactory;
-
-    protected $fillable = [
-        'user_id',
-        'photo_id',
-        'comment_text',
-    ];
-
-    //Relasi ke User: komentar ini dibuat oleh satu user
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    //Relasi ke Photo: komentar ini milik satu photo/post
-    public function photo()
-    {
-        return $this->belongsTo(Photo::class);
-    }
+    //
 }

255c5f5ba12dd9c7eb9c3ee520056ac8b523235b
hannahlrhl <hannahlarissahalim@gmail.com>
2025-06-14 04:32:02 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,26 +7,17 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
+     public function showProfile() {
+        return view('profile', ['user' => Auth::user()]);
     }
 
-    public function profile()
-    {
-        return view('user.profile', ['user' => auth()->user()]);
-    }
+    public function updateBio(Request $request) {
+        $request->validate(['bio' => 'nullable|string']);
 
-    public function updateBio(Request $request)
-    {
-        $request->validate([
-            'bio' => 'nullable|string|max:500',
-        ]);
-
-        $user = auth()->user();
+        $user = Auth::user();
         $user->bio = $request->bio;
         $user->save();
 
-        return back()->with('success', 'Bio updated successfully!');
+        return redirect('/profile')->with('success', 'Bio updated!');
     }
 }

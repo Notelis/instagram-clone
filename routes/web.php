7e1ab928f4293cac4a0aaad746cc47b0e1db838b
Notelis <rafeltheng@gmail.com>
2025-06-16 14:50:36 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Models\Photo;
 // Auth
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/register', [AuthController::class, 'showRegister']);
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout']);
 
